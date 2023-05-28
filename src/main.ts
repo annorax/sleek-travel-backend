22096f30070b8697a14387a75c92b8ef60d32dbf
annorax <ido.dovrat@gmail.com>
2023-05-28 20:09:32 +0300

    Integrate with express

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
+import express from 'express';
 import { createYoga } from 'graphql-yoga';
 import { createServer } from 'http';
 import { schema } from './schema';
 import { createContext } from './context';
  
 function main() {
+  const app = express();
   const yoga = createYoga({ schema, context: createContext });
-  const server = createServer(yoga);
-  server.listen(4000, () => {
-    console.info('Server is running on http://localhost:4000/graphql');
+  app.use(yoga.graphqlEndpoint, yoga);
+  app.listen(4000, () => {
+    console.log('Running a GraphQL API server at http://localhost:4000/graphql')
   });
 }
  
