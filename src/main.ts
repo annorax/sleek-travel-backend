ee34d62890578b55d398d423117660584b0e2a8a
annorax <ido.dovrat@gmail.com>
2023-05-28 13:40:44 +0300

    https://the-guild.dev/graphql/yoga-server/tutorial/basic/04-a-simple-query

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ function main() {
   const yoga = createYoga({ schema });
   const server = createServer(yoga);
   server.listen(4000, () => {
-    console.info('Server is running on http://localhost:4000/graphql')
+    console.info('Server is running on http://localhost:4000/graphql');
   });
 }
  
