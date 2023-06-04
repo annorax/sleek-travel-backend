be35ced6cf0f6a4a3b1802f4debe40330ad9ef49
annorax <ido.dovrat@gmail.com>
2023-06-04 14:55:40 +0300

    Add passport

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { createYoga } from 'graphql-yoga';
-import { createServer } from 'http';
 import { schema } from './schema';
 import { createContext } from './context';
+import passport from 'passport';
  
 function main() {
   const app = express();
+  app.use(passport.initialize());
   const yoga = createYoga({ schema, context: createContext });
   app.use(yoga.graphqlEndpoint, yoga);
   app.listen(4000, () => {
