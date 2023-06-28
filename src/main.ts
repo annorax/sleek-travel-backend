2df42c408025dcddf6f0fcfb0cd3a87cacab647f
annorax <ido.dovrat@gmail.com>
2023-06-28 11:01:27 +0300

    Minor refactoring

--- src/main.ts
+++ src/main.ts
@@ -3,60 +3,12 @@ import { ProductCrudResolver, applyResolversEnhanceMap } from "@generated/type-g
 import _ from "lodash";
 import express from "express";
 import { createYoga } from 'graphql-yoga';
-import { createContext, GraphQLContext } from './context';
+import { createContext } from './context';
 import passport from 'passport';
-import { Resolver, Args, buildSchema, Ctx, Mutation, Authorized } from "type-graphql";
-import { comparePassword, hashPassword, createTokenForUser, CustomAuthChecker } from "./auth";
-import { LogInUserArgs, LogInPayload, RegisterUserArgs, SafeUser } from "./types";
+import { buildSchema, Authorized } from "type-graphql";
+import { CustomAuthChecker } from "./auth";
 import { Role } from "@prisma/client";
-
-@Resolver(of => SafeUser)
-class CustomUserResolver {
-    @Mutation(returns => LogInPayload)
-    async registerUser(
-        @Ctx() { prisma }: GraphQLContext,
-        @Args() { name, email, password }: RegisterUserArgs,
-    ) : Promise<LogInPayload> {
-        const user = await prisma.user.create({
-            data: {
-                name,
-                email,
-                password: await hashPassword(password),
-                role: Role.NORMAL
-            }
-        });
-        const safeUser = _.omit(user, "password");
-        const token = createTokenForUser(user);
-        return { token, user: safeUser }
-    }
-
-    @Mutation(returns => LogInPayload, { nullable: true })
-    async logInUser(
-        @Ctx() { initialContext, prisma }: GraphQLContext,
-        @Args() { email, password }: LogInUserArgs,
-    ) : Promise<LogInPayload | null> {
-        let user = await prisma.user.findFirst({
-            where: { email }
-        });
-        if (!user) {
-            return null;
-        }
-        const passwordsMatch: boolean = await comparePassword(user.password, password);
-        if (!passwordsMatch) {
-            return null;
-        }
-        const ipAddress:string = initialContext.req.headers['x-forwarded-for'] || initialContext.req.socket.remoteAddress;
-        await prisma.login.create({
-            data: {
-                ...(ipAddress ? { ipAddress } : {}),
-                userId: user.id
-            }
-        });
-        const safeUser = _.omit(user, "password");
-        const token = createTokenForUser(user);
-        return { token, user: safeUser }
-    }
-}
+import { CustomUserResolver } from "./resolvers";
 
 async function main(): Promise<void> {
     const app = express();
---
+++ src/resolvers.ts
@@ -0,0 +1,55 @@
+import "reflect-metadata";
+import _ from "lodash";
+import { GraphQLContext } from './context';
+import { Resolver, Args, Ctx, Mutation } from "type-graphql";
+import { comparePassword, hashPassword, createTokenForUser } from "./auth";
+import { LogInUserArgs, LogInPayload, RegisterUserArgs, SafeUser } from "./types";
+import { Role } from "@prisma/client";
+
+@Resolver(of => SafeUser)
+export class CustomUserResolver {
+    @Mutation(returns => LogInPayload)
+    async registerUser(
+        @Ctx() { prisma }: GraphQLContext,
+        @Args() { name, email, password }: RegisterUserArgs,
+    ) : Promise<LogInPayload> {
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: await hashPassword(password),
+                role: Role.NORMAL
+            }
+        });
+        const safeUser = _.omit(user, "password");
+        const token = createTokenForUser(user);
+        return { token, user: safeUser }
+    }
+
+    @Mutation(returns => LogInPayload, { nullable: true })
+    async logInUser(
+        @Ctx() { initialContext, prisma }: GraphQLContext,
+        @Args() { email, password }: LogInUserArgs,
+    ) : Promise<LogInPayload | null> {
+        let user = await prisma.user.findFirst({
+            where: { email }
+        });
+        if (!user) {
+            return null;
+        }
+        const passwordsMatch: boolean = await comparePassword(user.password, password);
+        if (!passwordsMatch) {
+            return null;
+        }
+        const ipAddress:string = initialContext.req.headers['x-forwarded-for'] || initialContext.req.socket.remoteAddress;
+        await prisma.login.create({
+            data: {
+                ...(ipAddress ? { ipAddress } : {}),
+                userId: user.id
+            }
+        });
+        const safeUser = _.omit(user, "password");
+        const token = createTokenForUser(user);
+        return { token, user: safeUser }
+    }
+}
